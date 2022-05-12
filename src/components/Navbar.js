1e53731514df7d9d9ab4749c67b9a5ef7ca3ee28
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-12 14:25:35 +0200

    Travel Journal React

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                <a hraf="#"> 🌏 My travel Jornal</a>
+            </div>
+        </nav>
+    );
+}
